2725273b2d6ab019d716b83e47785f8265368483
Guy Rodrigue Koffi <gr.koffi@prismamedia.com>
2017-06-16 14:15:04 +0200

    Add few improvements
    - Verify and validate inputs
    - Better args parsing
    - Modularize script
    - Remove backup history

--- aws-simple-efs-backup/cfn/backup.sh
+++ aws-simple-efs-backup/cfn/backup.sh
@@ -1,38 +1,69 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+
+set -e
 
 stack_name=$1
 source_efs=$2
 destination_efs=$3
 region=$4
-retain=10
-efsid=$source_efs
+
+
+echo "Checking script parameters"
+verify_args(){
+    if [ -z ${2+x} ]; then
+        echo "$1 is missing, aborting backup"
+        return 1
+    fi
+}
+
+validate_path(){
+    if [[ $1 == *"../"* ]]; then
+        return 1
+    fi
+
+    if [[ $1 == "/" ]]; then
+        return 1
+    fi
+}
+
+mount_efs(){
+    efsid=$1
+    mountpoint=$2
+    region=$3
+
+    mount -t nfs4 \
+        -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \
+        "${efsid}.efs.${region}.amazonaws.com:/" ${mountpoint}
+}
+
+verify_args "stack_name" ${stack_name}
+verify_args "source_efs" ${source_efs}
+verify_args "destination_efs" ${destination_efs}
+verify_args "region" ${region}
+
+validate_path ${source_efs}
+validate_path ${destination_efs}
+
 
 echo "Running backup script"
 
-mkdir /backup
-mkdir /mnt/backups
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $source_efs.efs.eu-west-1.amazonaws.com:/ /backup
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $destination_efs.efs.eu-west-1.amazonaws.com:/ /mnt/backups
+source_mount="/backup"
+dest_mount="/mnt/backup"
+mkdir -p ${source_mount}
+mkdir -p ${dest_mount}
 
-# we need to calculate date - retain to know directory we have to remove
-let "retain=$retain-1"
-if test -d /mnt/backups/$efsid/$interval.$retain; then
-  rm -rf /mnt/backups/$efsid/$interval.$retain
-fi
+mount_efs ${source_efs} ${source_mount} ${region}
+mount_efs ${destination_efs} ${dest_mount} ${region}
 
 # First create directory if doesn't exist
-if [ ! -d /mnt/backups/$efsid ]; then
-  mkdir -p /mnt/backups/$efsid
-  chmod 700 /mnt/backups/$efsid
-fi
-if [ ! -d /mnt/backups/$efsid/efsbackup-logs ]; then
-  mkdir -p /mnt/backups/$efsid/efsbackup-logs
-  chmod 700 /mnt/backups/$efsid/efsbackup-logs
-fi
+mkdir -p -m 700 ${dest_mount}/$source_efs/{,efsbackup-logs}
 
-rm -f /tmp/efs-backup.log
-rsync -ah --stats --delete --numeric-ids --log-file=/tmp/efs-backup.log /backup/ /mnt/backups/$efsid/`date +%Y%m%d-%H%M`/
-cp /tmp/efs-backup.log /mnt/backups/$efsid/efsbackup-logs/$efsid-`date +%Y%m%d-%H%M`.log
+curr_date=$(date +%Y%m%d-%H%M)
+logfile="${dest_mount}/${source_efs}/efsbackup-logs/${source_efs}-${curr_date}.log"
 
-echo "removing cloudformation stack"
-aws cloudformation delete-stack --stack-name $stack_name --region $region
+# Back up files
+rsync -ah --stats --delete --numeric-ids --log-file="${logfile}" \
+    ${source_mount} ${dest_mount}/${source_efs}/${curr_date}
+
+echo "Removing cloudformation stack"
+aws cloudformation delete-stack --stack-name ${stack_name} --region ${region}
--- aws-simple-efs-backup/efs-backup.sh
+++ aws-simple-efs-backup/efs-backup.sh
@@ -1,68 +1,85 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-script_name=$(basename $0)
+# set -ex
+set -e
+
 
 usage(){
-  echo "Usage: $script_name --source source-efs-id --dest dest-efs-id --subnet-id subnet-id [--instance-type t2.xlarge]" >&2
-  exit 1
+    echo "usage: ${0##*/} -s source-efs -d dest-efs -S subnet_id [-i instance_type]"
+    return 1
 }
 
-parse_args(){
-  while [[ $# -gt 0 ]]
-  do
-    key="$1"
-    case $key in
-      --source)
-        source_efs=$2
-        shift
-        ;;
-      --dest)
-        destination_efs=$2
-        shift
-        ;;
-      --subnet-id)
-        subnet_id=$2
-        shift
-        ;;
-      --instance-type)
-        instance_type=$2
-        shift
-        ;;
-      *)
-        usage
-        ;;
+verify_args(){
+    if [ -z ${2+x} ]; then
+        echo "$1 is mandatory"
+        return 1
+    fi
+}
+
+get_mount_target_id(){
+    local efs_id=$1
+    local subnet_id=$2
+
+    aws efs describe-mount-targets \
+        --file-system-id ${efs_id} \
+        --output text \
+        --query "MountTargets[?SubnetId==\`${subnet_id}\`].MountTargetId"
+}
+
+get_mount_target_sg(){
+    local mount_target_id=$1
+
+    aws efs describe-mount-target-security-groups \
+        --mount-target-id ${mount_target_id} \
+        --query "SecurityGroups" \
+        --output text
+}
+
+get_stack_status(){
+  local stack_name=$1
+  aws cloudformation describe-stacks \
+    --stack-name ${stack_name} \
+    --query 'Stacks[0].StackStatus' \
+    --output text
+}
+
+command -v aws >/dev/null 2>&1 ||
+    { echo >&2 "awscli is missing. Aborting ..."; exit 1; }
+
+while getopts "s:d:S:i:" arg; do
+    case ${arg} in
+        s) source_efs=${OPTARG} ;;
+        d) destination_efs=${OPTARG} ;;
+        S) subnet_id=${OPTARG} ;;
+        i) instance_type=${OPTARG} ;;
+        *) usage ;;
     esac
-    shift
-  done
-}
+done
 
-
-[ $# -ge 6 ] || usage
-[ $(( $# % 2  )) -eq 0 ] || usage
-parse_args "$@"
-aws_account_id=$(aws sts get-caller-identity --query "Account" --output text)
-
-cfn_stack_name="efs-backup"
 instance_type=${instance_type:-"t2.micro"}
+vpc_id=$(aws ec2 describe-subnets \
+    --subnet-ids ${subnet_id} \
+    --output text \
+    --query "Subnets[0].VpcId")
 
-vpc_id=$(aws ec2 describe-subnets --subnet-ids $subnet_id --output text --query "Subnets[0].VpcId")
+source_mount_target_id=$(get_mount_target_id ${source_efs} ${subnet_id})
+dest_mount_target_id=$(get_mount_target_id ${destination_efs} ${subnet_id})
 
-source_mount_target_id=$(aws efs describe-mount-targets --file-system-id $source_efs --output text --query "MountTargets[?SubnetId==\`$subnet_id\`].MountTargetId")
-dest_mount_target_id=$(aws efs describe-mount-targets --file-system-id $destination_efs --output text --query "MountTargets[?SubnetId==\`$subnet_id\`].MountTargetId")
+source_efs_sg=$(get_mount_target_sg ${source_mount_target_id})
+dest_efs_sg=$(get_mount_target_sg ${dest_mount_target_id})
 
-source_efs_sg=$(aws efs describe-mount-target-security-groups --mount-target-id $source_mount_target_id --query "SecurityGroups" --output text)
-dest_efs_sg=$(aws efs describe-mount-target-security-groups --mount-target-id $dest_mount_target_id --query "SecurityGroups" --output text)
-
+curr_date=$(date +%Y%m%d-%H%M)
 aws cloudformation deploy \
-  --template-file cfn/backup.yml \
-  --stack-name $cfn_stack_name \
-  --parameter-overrides \
-  InstanceType="$instance_type" \
-  SourceEFS="$source_efs" \
-  DestinationEFS="$destination_efs" \
-  SubnetId="$subnet_id" \
-  VpcId="$vpc_id" \
-  SourceMountSG="$source_efs_sg" \
-  DestinationMountSG="$source_efs_sg" \
-  --capabilities CAPABILITY_IAM
-exit 0
+    --stack-name "aws-simple-efs-backup-${curr_date}" \
+    --template-file cfn/backup.yml \
+    --parameter-overrides \
+    InstanceType=${instance_type} \
+    SourceEFS=${source_efs} \
+    DestinationEFS=${destination_efs} \
+    SubnetId=${subnet_id} \
+    VpcId=${vpc_id} \
+    SourceMountSG=${source_efs_sg} \
+    DestinationMountSG=${source_efs_sg} \
+    --capabilities CAPABILITY_IAM
+
+echo "Backing up in progress, please check cloudformation logs"
